200599926deea8140f6ac5f7cb5d6f07d1e2023c
yasu <mashiko1101@gmail.com>
2025-03-28 16:40:43 +0900

    初期ページのindexにCSSを追加

--- src/app/index.php
+++ src/app/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>レシピ管理アプリ</title>
+    <link rel="stylesheet" href="index.css">
+
 </head>
 <body>
     <h1>レシピ管理アプリへようこそ</h1>
